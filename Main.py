a3343a4aeb62e05d6df27ac7f4403e481af16053
MUY SENGLY <muysengly@gmail.com>
2025-05-30 14:58:28 +0700

    update

---
+++ Main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+# os.chdir("./resource/view_controller/login_form/")
+os.chdir("./resource/view_controller/main_application_form/")
+
+os.system("python Controller.py")
